71d477193141a2c440fd3674807d78b9a4bb8909
Daniel Junior <oidanieljunior@gmail.com>
2019-07-22 19:43:02 -0300

    Navbar collapse method

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -2,7 +2,13 @@
   <nav class="navbar navbar-expand-md navbar-light bg-light">
     <div class="container">
       <a class="navbar-brand" href="/">{{ siteTitle }}</a>
-
+      <button class="navbar-toggler d-lg-none" type="button"
+        data-toggle="collapse" :data-target="'#' + menuName"
+        :aria-controls="menuName"
+        aria-expanded="false" aria-label="Exibir/ocultar navegação"
+        v-on:click.prevent="toggleCollapse">
+        <span class="navbar-toggler-icon"></span>
+      </button>
       <NavbarCollapse :menuName="menuName" :links="links" />
     </div>
   </nav>
@@ -20,8 +26,14 @@ export default {
     siteTitle: String,
     menuName: String,
   },
+  methods: {
+    toggleCollapse() {
+      this.collapseShow = !this.collapseShow;
+    },
+  },
   data() {
     return {
+      collapseShow: false,
       links: [
         {
           name: 'Home',
--- src/components/Navbar/NavbarCollapse.vue
+++ src/components/Navbar/NavbarCollapse.vue
@@ -1,19 +1,11 @@
 <template>
-  <div>
-    <button class="navbar-toggler d-lg-none" type="button"
-      data-toggle="collapse" :data-target="'#' + menuName"
-      :aria-controls="menuName"
-      aria-expanded="false" aria-label="Exibir/ocultar navegação">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" :id="menuName">
+    <div class="collapse navbar-collapse" :class="{show: $parent.collapseShow}" :id="menuName">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item" v-for="(link, index) in links" :key="index">
           <a class="nav-link" :href="link.route">{{ link.name }}</a>
         </li>
       </ul>
     </div>
-  </div>
 </template>
 
 <script>
