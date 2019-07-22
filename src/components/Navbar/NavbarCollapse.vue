e951aed265ce3cbda28a4cd1451561217dd3e850
Daniel Junior <oidanieljunior@gmail.com>
2019-07-22 20:05:06 -0300

    Navbar Collapse component style adjust

--- src/components/Navbar/NavbarCollapse.vue
+++ src/components/Navbar/NavbarCollapse.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="collapse navbar-collapse" :class="{show: $parent.collapseShow}" :id="menuName">
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <ul class="navbar-nav text-center ml-auto mt-2 mt-lg-0">
         <li class="nav-item" v-for="(link, index) in links" :key="index">
           <a class="nav-link" :href="link.route">{{ link.name }}</a>
         </li>
